4fc6aee5be4383d5eff26d40ffb741e2dfa4913b
AlehaWP <nerchenko@gmail.com>
2022-08-22 16:01:16 +0300

    исправляет на стрелку

--- src/presenter/trip-events-presenter.js
+++ src/presenter/trip-events-presenter.js
@@ -5,21 +5,20 @@ import EditPointView from '../view/edit-point-view.js';
 import AddPointView from '../view/add-point-view';
 import PointView from '../view/point-view';
 
-const PointCount = 3;
-
-
 export default class TripEventsPresenter {
+  pointCount = 3;
   sort = new SortView();
   pointList = new PointListView();
 
-  init(container) {
-    render(this.sort, container);
+  init = (container) => {
+    this.container = container;
+    render(this.sort, this.container);
     render(this.pointList, this.sort.getElement(), RenderPosition.AFTEREND);
     render(new EditPointView(), this.pointList.getElement());
     render(new AddPointView(), this.pointList.getElement());
 
-    for (let i = 0; i <= PointCount; i++) {
+    for (let i = 0; i <= this.pointCount; i++) {
       render(new PointView(), this.pointList.getElement());
     }
-  }
+  };
 }
