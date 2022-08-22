804a9059c699108383ba84f363e0b9301904885f
AlehaWP <nerchenko@gmail.com>
2022-08-23 00:58:55 +0300

    запихнул число в константу

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,7 +1,9 @@
 import { generatePoint } from '../mock/point.js';
 
+const POINT_COUNT = 20;
+
 export default class PointsModel {
-  points = Array.from({length:20}, generatePoint);
+  points = Array.from({length:POINT_COUNT}, generatePoint);
 
   getPoints = () => this.points;
 
